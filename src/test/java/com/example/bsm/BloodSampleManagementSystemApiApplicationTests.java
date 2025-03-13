67a0ffaa71fe4cc80945a4648b9d50ae39575f09
Mandakini-Patil <mandakinipatil2002@gmail.com>
2025-03-13 21:41:41 +0530

    mainclassfile

---
+++ src/test/java/com/example/bsm/BloodSampleManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodSampleManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
